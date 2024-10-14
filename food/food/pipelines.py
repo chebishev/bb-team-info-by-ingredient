af15f51c4fdaf2d8a7a86e521cced56472e814e8
chebishev <atanas.chebishev@gmail.com>
2024-10-14 14:18:17 +0300

    Initial commit

---
+++ food/food/pipelines.py
@@ -0,0 +1,28 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+class FoodPipeline:
+    def process_item(self, item, spider):
+        quantity = item.get("quantity")
+
+        # split the number and the unit
+        quantity_list = quantity.split()
+        # remove "," from the number
+        quantity = quantity_list[0].replace(",", "")
+        # assign the unit into a variable
+        unit = quantity_list[1]
+        # cast to number
+        if "." in quantity:
+            item["quantity"] = float(quantity)
+        else:
+            item["quantity"] = int(quantity)
+        item["unit"] = unit
+
+        return item
---
+++ food/food/spiders/ingredients.py
@@ -0,0 +1,56 @@
+import scrapy
+from food.items import FoodItem
+from scrapy.settings.default_settings import FEED_EXPORT_FIELDS
+
+KEYWORD = "Калций"
+
+
+class IngredientsSpider(scrapy.spiders.SitemapSpider):
+    name = "ingredients"
+    sitemap_urls = [
+        "https://www.bb-team.org/sitemaps/foods",
+    ]
+
+    custom_settings = {
+        "FEED_EXPORT_FIELDS": ["name", "description", "quantity", "unit", "url"],
+        "FEEDS": {f"{name}.csv": {"format": "csv", "overwrite": True}},
+        "LOG_LEVEL": "WARNING",
+        "LOG_FILE": f"log_{name}.txt",
+        "ITEM_PIPELINES": {"food.pipelines.FoodPipeline": 300},
+    }
+
+    def parse(self, response):
+        name = response.css("h1::text").get().strip()
+        description = response.css("h1+p::text").get()
+        url = response.url
+        quantity = ""
+        tables = response.css("h2+table")
+        keyword_found = False
+
+        for table in tables:
+            if keyword_found:
+                break
+            current_table = table.css("table>tbody")
+            for row in current_table:
+                if keyword_found:
+                    break
+                found_ingredients = row.css("tr")
+                for ingredient in found_ingredients:
+                    current_ingredient = ingredient.css("a::text").get()
+                    if current_ingredient == KEYWORD:
+                        quantity = ingredient.css("td::text").get()
+                        keyword_found = True
+                        break
+
+        if not quantity.strip() or quantity is None:
+            return
+
+        food_item = FoodItem(
+            name=name,
+            description=description.strip() if description else "",
+            quantity=quantity,
+            unit="",
+            url=url
+        )
+
+        yield food_item
