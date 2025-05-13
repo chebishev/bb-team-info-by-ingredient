3c2b18f8fe26d649c16799c12d1b38a3a712dd41
chebishev <atanas.chebishev@gmail.com>
2025-05-13 16:05:45 +0300

    no gluten filter

--- food/food/pipelines.py
+++ food/food/pipelines.py
@@ -11,18 +11,18 @@ from itemadapter import ItemAdapter
 class FoodPipeline:
     def process_item(self, item, spider):
         quantity = item.get("quantity")
-
-        # split the number and the unit
-        quantity_list = quantity.split()
-        # remove "," from the number
-        quantity = quantity_list[0].replace(",", "")
-        # assign the unit into a variable
-        unit = quantity_list[1]
-        # cast to number
-        if "." in quantity:
-            item["quantity"] = float(quantity)
-        else:
-            item["quantity"] = int(quantity)
-        item["unit"] = unit
+        if quantity:
+            # split the number and the unit
+            quantity_list = quantity.split()
+            # remove "," from the number
+            quantity = quantity_list[0].replace(",", "")
+            # assign the unit into a variable
+            unit = quantity_list[1]
+            # cast to number
+            if "." in quantity:
+                item["quantity"] = float(quantity)
+            else:
+                item["quantity"] = int(quantity)
+            item["unit"] = unit
 
         return item
--- food/food/spiders/ingredients.py
+++ food/food/spiders/ingredients.py
@@ -11,7 +11,7 @@ warnings.filterwarnings("ignore", category=scrapy.exceptions.ScrapyDeprecationWa
 
 logger = logging.getLogger(__name__)
 
-KEYWORD = "Калций"
+# KEYWORD = "Калций"
 FOOD_GROUPS_TO_EXCLUDE = ["Свински продукти", "Млечни и яйчни продукти", "Захарни изделия"]
 INGREDIENTS_TO_EXCLUDE = ["пиле", "захар"]
 
@@ -35,38 +35,40 @@ class IngredientsSpider(scrapy.spiders.SitemapSpider):
     def parse(self, response):
         name = response.css("h1::text").get().strip()
         description = response.css("h1+p::text").get()
+        if "без глутен" not in description.lower():
+            return
         url = response.url
         # Escape temporary AttributeError, because of .lower() method
-        if url == "https://www.bb-team.org/hrani":
-            return
-        for product in INGREDIENTS_TO_EXCLUDE:
-            if product in name.lower() or product in description.lower():
-                return
+        # if url == "https://www.bb-team.org/hrani":
+        #     return
+        # for product in INGREDIENTS_TO_EXCLUDE:
+        #     if product in name.lower() or product in description.lower():
+        #         return
         food_group = response.css("nav>ol>li:last-child a::text").get().strip()
         if food_group in FOOD_GROUPS_TO_EXCLUDE:
             return
         
-        quantity = ""
-        tables = response.css("h2+table")
-        keyword_found = False
+        quantity = []
+        # tables = response.css("h2+table")
+        # keyword_found = False
 
-        for table in tables:
-            if keyword_found:
-                break
-            current_table = table.css("table>tbody")
-            for row in current_table:
-                if keyword_found:
-                    break
-                found_ingredients = row.css("tr")
-                for ingredient in found_ingredients:
-                    current_ingredient = ingredient.css("a::text").get()
-                    if current_ingredient == KEYWORD:
-                        quantity = ingredient.css("td::text").get()
-                        keyword_found = True
-                        break
+        # for table in tables:
+        #     if keyword_found:
+        #         break
+        #     current_table = table.css("table>tbody")
+        #     for row in current_table:
+        #         if keyword_found:
+        #             break
+        #         found_ingredients = row.css("tr")
+        #         for ingredient in found_ingredients:
+        #             current_ingredient = ingredient.css("a::text").get()
+        #             if current_ingredient == KEYWORD:
+        #                 quantity = ingredient.css("td::text").get()
+        #                 keyword_found = True
+        #                 break
 
-        if not quantity.strip() or quantity is None:
-            return
+        # if not quantity.strip() or quantity is None:
+        #     return
 
         food_item = FoodItem(
             name=name,
