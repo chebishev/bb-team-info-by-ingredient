dc47e71184058f652aeb05caede1b230b5251185
chebishev <atanas.chebishev@gmail.com>
2025-05-23 09:18:56 +0300

    remove database and all connected files: db, models, .env, env_sample update requirements

--- food/food/create_db.py
+++
@@ -1,9 +0,0 @@
-from models import Base
-from db import engine
-
-def main():
-    Base.metadata.create_all(engine)
-    print("✅ Database schema created.")
-
-if __name__ == "__main__":
-    main()
