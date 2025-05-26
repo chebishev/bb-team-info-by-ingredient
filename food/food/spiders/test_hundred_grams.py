ae78e2116e8742b8c8c15fb5af56fb8b5e30d9ed
chebishev <atanas.chebishev@gmail.com>
2025-05-26 12:51:20 +0300

    add second spider to test the new Hundred Grams group

--- food/food/spiders/test_hundred_grams.py
+++ food/food/spiders/test_hundred_grams.py
@@ -14,18 +14,21 @@ class HundredGramsSpider(SitemapSpider):
         "FEEDS": {
             f"{name}.csv": {"format": "csv", "overwrite": True}
         },
-        "FEED_EXPORT_FIELDS": ["Serving Size", "calories", "proteine", "carbonates", "fats"],
+        "FEED_EXPORT_FIELDS": ["Serving Size", "nutritions"],
         "LOG_FILE": f"log_{name}.txt"
     }
 
-    def _parse(self, response):
-        serving_size_list = response.css("p.font-semibold ::text").getall()
-        serving_size = " ".join(text.strip() for text in serving_size_list).strip()
+    def parse(self, response):
+        if response.url == "https://www.bb-team.org/hrani":
+            logger.info("Skipping the main food page.")
+            return
+        
+        serving_size = "100 грама съдържат:"
+        nutritions = response.css("p.font-semibold+div")
+        nutritions_string = " ".join(n.strip() for n in nutritions.css("div span::text").getall()).replace("г", "")
+        nutritions_list = " ".join(nutritions_string.split()).split()
 
         yield {
-            "Serving Size": serving_size_list[0] if serving_size_list else None,
-            "calories": response.css("p.text-2xl::text").get(),
-            "proteine": response.css("p.text-2xl+ p::text").get(),
-            "carbonates": response.css("p.text-2xl+ p+ p::text").get(),
-            "fats": response.css("p.text-2xl+ p+ p+ p::text").get(),
+            "Serving Size": serving_size,
+            "nutritions": f"{nutritions_list} -> {len(nutritions_list)} елемента",
         }
